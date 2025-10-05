544c0b64798b2d76fecb418254af92026bdcd229
Bumblebee202111 <32139776+Bumblebee202111@users.noreply.github.com>
2025-10-06 07:34:15 +0800

    chore: Bump version to 1.1.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -37,7 +37,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Exe)
             packageName = "com.github.bumblebee202111.deapi"
-            packageVersion = "1.0.0"
+            packageVersion = "1.1.0"
         }
     }
 }
